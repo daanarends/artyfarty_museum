10420eaa75161488603c29d623cce9537136c189
daanarends <32448359+daanarends@users.noreply.github.com>
2017-10-07 12:43:38 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :museums
+
+  root "arts#index"
+
+  resources :arts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
